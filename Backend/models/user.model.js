fd54869d477d0ee4e45c4dce756f253916b3c7b9
Asus <abhijeet.jmt108@gmail.com>
2024-12-04 11:16:50 +0530

    added user.model and user.controller

---
+++ Backend/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 character long'],
+
+        },
+        lastname:{
+            type: String,
+            minlength: [3, 'last name must be at least 3 character long'],
+        }
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [6, 'Email must be at least 6 character long']
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId:{
+        type: String,
+    },
+})
+
+userSchema.method.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.method.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.static.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+
+module.exports = userModel;
